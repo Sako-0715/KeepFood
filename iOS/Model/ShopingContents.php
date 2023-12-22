350b6fbf2155de5a65924462d774f9903d92b866
sako-0715 <sakou.0715@gmail.com>
2023-12-22 19:05:31 +0900

    ModelとControllerの追加

---
+++ iOS/Model/ShopingContents.php
@@ -0,0 +1,20 @@
+<?php
+    $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+$sql = "SELECT * FROM SHOPING_CONTENTS";
+$result = $db->query($sql);
+
+$dataArray = [];
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $row['PRICE'] = intval($row['PRICE']);
+        $dataArray[] = $row;
+    }
+    $js = json_encode($dataArray);
+    error_log(print_r($js,true));
+    echo $js;
+} else {
+    error_log("クエリ実行エラー: " . $db->error);
+}
+
+$db->close();
